bfdaf1d53813da50447cfc591afa93837cfeebe8
jdickie <jgrantd@gmail.com>
2012-02-11 16:45:21 -0500

    changing domain pointers

--- wikicite/js/client.js
+++ wikicite/js/client.js
@@ -5,13 +5,21 @@ to generate a wiki citation
 (function($) {
 	$(function() {
 		var wikicite = new WikiCite({}),
-		hash, outputPattern, citation;
+		hash, outputPattern, citation,
+		domain, m;
+		
+		// get domain info
+		// var m = window.location.match(/^http:\/\/[^\/]+/);
+		// domain = m[0];
+		
 		
 		// get record fields hash from server
 		hash = $.ajax({
-			url: '',
-			type: 'get'
+			url: '/citations/get_citation_method_by_domain',
+			type: 'get',
+			data: 'domain=aleph.nkp.cz'
 		}).responseText;
+		console.log('return from server ' + hash);
 		// set the root element where the information we want is
 		wikicite.setRootElement('table#record');
 		
@@ -21,7 +29,7 @@ to generate a wiki citation
 		wikicite.parse();
 		
 		// get the citation string
-		citation = wikicite.getCitation('"%sTitle%s" by %sAuthor%s');
+		citation = wikicite.getCitation('"%sTitle%s" by %sAuthor%s', '%s');
 		
 		// get output string and display
 		$('#output_display').text(citation);
