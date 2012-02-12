ec09a9a028f2047f4e265b7b664560950cf15fad
Asaf Bartov <asaf.bartov@gmail.com>
2012-02-12 05:59:17 -0800

    - added CitationTemplate seed data (for Czech Wikipedia)

--- sitos/db/seeds.rb
+++ sitos/db/seeds.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -11,3 +12,5 @@ ScrapingElement.create("scraping_algorithm_id"=>algo, "marc_key"=>"245", "hash_k
 ScrapingElement.create("scraping_algorithm_id"=>algo, "marc_key"=>"260", "hash_key"=>"PlacePublisherDate")
 ScrapingElement.create("scraping_algorithm_id"=>algo, "marc_key"=>"250", "hash_key"=>"Edition")
 CitableSite.create("domain" => "aleph.nkp.cz", "scraping_algorithm_id" => algo)
+CitationTemplate.create("name" => "Czech Wikipedia", "domain" => "cs.wikipedia.org", "pattern" => "{{Citace monografie | příjmení = @@WIKICITE:last_name@@ | jméno = @@WIKICITE:first_name@@ | titul = @@WIKICITE:title@@ | vydavatel = @@WIKICITE:publisher@@ | místo = @@WIKICITE:location@@ | rok = @@WIKICITE:year@@ | počet stran = @@WIKICITE:number_of_pages@@ | strany = @@WIKICITE:pages@@ | isbn = @@WIKICITE:isbn@@ | jazyk = @@WIKICITE:language@@ }}")
+
