dd01708e825fe5fc5c7ba1375b8cd18c0a1bd782
Asaf Bartov <asaf.bartov@gmail.com>
2012-03-03 19:03:31 -0700

    - added nil checks

--- sitos/app/controllers/citations_controller.rb
+++ sitos/app/controllers/citations_controller.rb
@@ -2,9 +2,11 @@ class CitationsController < ApplicationController
   def get_citation_info_by_domain
     site = CitableSite.find_by_domain(params[:domain])
     @marc_tags = {}
-    site.scraping_algorithm.scraping_elements.each {|e|
-      @marc_tags[e.marc_key] = e.hash_key
-    }
+    unless site.nil? or site.scraping_algorithm.nil? or site.scraping_algorithm.scraping_elements.empty?
+      site.scraping_algorithm.scraping_elements.each {|e|
+        @marc_tags[e.marc_key] = e.hash_key
+      }
+    end
   end
   def get_citation_pattern_by_language
     @pattern = CitationTemplate.find_by_domain(params[:lang]+'.wikipedia.org') # TODO: un-hardcode
