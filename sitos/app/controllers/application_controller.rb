a437390fd77dc2dfa85f2de5b73c4308f31e5279
Asaf Bartov <asaf.bartov@gmail.com>
2012-04-20 14:46:48 -0700

    Fixed long-standing silly capitalization bug in CORS handling

--- sitos/app/controllers/application_controller.rb
+++ sitos/app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ def cors_preflight_check
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
     headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version,Accept,Origin'
-  if request.method == :options
+  if request.method.casecmp("OPTIONS") == 0
     headers['Access-Control-Max-Age'] = '1728000'
     render :text => '', :content_type => 'text/plain'
   end
